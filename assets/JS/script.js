f939c46cc6c2b8d47658a6d653da92e7180406d3
pthorkan <pthorkan@gmail.com>
2022-06-29 14:59:25 +0000

    Saving to refresh

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,1 +1,31 @@
-console.log("Hi");
+// Waits for DOM to finish loading before running the game
+
+document.addEventListener("DOMContentLoaded", function() {
+    let buttons = document.getElementsByTagName("button");
+    console.log("buttons")
+    for(let button of buttons) {
+        button.addEventListener("click", function() {
+            if (this.getAttribute("data-type") === "rock") {
+                alert("You clicked Rock");
+            } else {
+                alert(`You clicked $(data-type)!`);
+            }
+        })
+    }
+})
+
+function runGame() {
+
+}
+
+function checkAnswer() {
+
+}
+
+function incrementScore() {
+
+}
+
+function incrementWrongAnswer() {
+
+}
