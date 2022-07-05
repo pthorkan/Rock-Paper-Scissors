5ec0f4b6203074e37d42187bb8dd139b3c4ac6c4
pthorkan <pthorkan@gmail.com>
2022-07-05 17:10:41 +0000

    Add script.js for score area

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -14,11 +14,13 @@ function runGame() {
         button.addEventListener("click", function() {
         let user1 = this.getAttribute("data-type");
         console.log(user1);
+        appendUserSelection(user1);
         let computer1 = randomChoice();
         console.log(computer1);
         
         if (user1 == computer1) {
-            console.log("Tie")
+            console.log("Tie");
+            appendSelection(computer1);
         } else if (user1 == "rock") {
             rockArray(computer1);
         } else if (user1 == "paper") {
@@ -33,19 +35,19 @@ function runGame() {
 
 function rockArray(computer1) {
     console.log(computer1);
-    let array = ["paper", "rock", "scissors"];
+    appendSelection(computer1);
     if (computer1 =="paper" ? (console.log("Computer Wins"), incrementWrongAnswer()) : (console.log("User Wins"), incrementScore())); 
 }
 
 function paperArray(computer1) {
     console.log(computer1);
-    let array = ["scissors", "paper", "rock"];
+    appendSelection(computer1);
     if (computer1 =="scissors" ? (console.log("Computer Wins"), incrementWrongAnswer()) : (console.log("User Wins"), incrementScore())); 
 }
  
 function scissorsArray(computer1) {
     console.log(computer1);
-    let array = ["rock", "scissors", "paper"];
+    appendSelection(computer1);
     if (computer1 =="rock" ? (console.log("Computer Wins"), incrementWrongAnswer()) : (console.log("User Wins"), incrementScore())); 
 }
 
@@ -68,9 +70,7 @@ function incrementScore() {
     if (newScore == 5) {
         alert("Whooop...you won!!");
         resetScore();
-    }
-
-
+    };
 }
 
 function incrementWrongAnswer() {
@@ -88,4 +88,30 @@ function incrementWrongAnswer() {
 function resetScore() {
     document.getElementById("games-lost").innerText = 0;
     document.getElementById("games-won").innerText = 0;
+}
+
+function appendSelection (computer1) {
+    let div = document.createElement('div');
+    if (computer1 == "paper") {
+        div.innerHTML = '<i class="fa-regular fa-hand"></i>';
+    } else if (computer1 == "rock") {
+        div.innerHTML = '<i class="fa-regular fa-hand-back-fist"></i>';
+    } else if (computer1 == "scissors") {
+        div.innerHTML = '<i class="fa-regular fa-hand-scissors"></i>';
+    };
+    div.classList.add("selection");
+    document.getElementById("column3").appendChild(div);
+}
+
+function appendUserSelection(user1) {
+    let div = document.createElement('div');
+    if (user1 == "paper") {
+        div.innerHTML = '<i class="fa-regular fa-hand"></i>';
+    } else if (user1 == "rock") {
+        div.innerHTML = '<i class="fa-regular fa-hand-back-fist"></i>';
+    } else if (user1 == "scissors") {
+        div.innerHTML = '<i class="fa-regular fa-hand-scissors"></i>';
+    };
+    div.classList.add("selection");
+    document.getElementById("column1").appendChild(div);
 }
